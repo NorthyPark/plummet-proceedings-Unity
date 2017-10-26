3abb23017d6665820686ba8698dcd571085c9471
NorthyPark <32385215+NorthyPark@users.noreply.github.com>
2017-10-26 18:37:54 +0200

    Inital release

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour
+{
+
+    public static GameMaster gm;
+
+    void Awake()
+    {
+        if (gm == null)
+        {
+            gm = this;
+        }
+    }
+
+    public Transform playerPrefab;
+
+    public CameraShake cameraShake;
+
+    void Start()
+    {
+        if (cameraShake == null)
+        {
+            Debug.LogError("No camera shake referenced in GameMaster");
+        }
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+    }
+
+    public static void KillEnemy(Enemy enemy)
+    {
+        gm._KillEnemy(enemy);
+    }
+    public void _KillEnemy(Enemy _enemy)
+    {
+        cameraShake.Shake(_enemy.shakeAmt, _enemy.shakeLength);
+        Destroy(_enemy.gameObject);
+    }
+
+}
